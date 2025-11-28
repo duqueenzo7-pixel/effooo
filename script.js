93bb73261b0f0f679d6ea56afd9912daeb69064f
duqueenzo7-pixel <duqueenzo7@gmail.com>
2025-11-28 22:33:49 +0000

    ehehehehfdfdsd

--- script.js
+++ script.js
@@ -27,3 +27,72 @@ if (themeButton3) {
         themeButton3.classList.toggle('rouge');
     });
 }
+
+//---------------------------------------------------------
+
+
+
+const themeButton4 = document.querySelector('#bouton4');
+
+if (themeButton4) {  
+    themeButton4.addEventListener('click', () => {
+        themeButton4.classList.toggle('rouge');
+    });
+}
+
+
+//---------------------------------------------------------
+
+const themeButton5 = document.querySelector('#bouton5');
+
+if (themeButton5) {  
+    themeButton5.addEventListener('click', () => {
+        themeButton5.classList.toggle('vert');
+    });
+}
+
+
+//---------------------------------------------------------
+
+
+const themeButton6 = document.querySelector('#bouton6');
+
+if (themeButton6) {  
+    themeButton6.addEventListener('click', () => {
+        themeButton6.classList.toggle('rouge');
+    });
+}
+
+
+//---------------------------------------------------------
+
+const themeButton7 = document.querySelector('#bouton7');
+
+if (themeButton7) {  
+    themeButton7.addEventListener('click', () => {
+        themeButton7.classList.toggle('rouge');
+    });
+}
+
+//---------------------------------------------------------
+
+const themeButton8 = document.querySelector('#bouton8');
+
+if (themeButton8) {  
+    themeButton8.addEventListener('click', () => {
+        themeButton8.classList.toggle('rouge');
+    });
+}
+
+//---------------------------------------------------------
+
+
+const themeButton9 = document.querySelector('#bouton9');
+
+if (themeButton9) {  
+    themeButton9.addEventListener('click', () => {
+        themeButton9.classList.toggle('vert');
+    });
+}
+
+//---------------------------------------------------------
